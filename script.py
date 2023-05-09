40ea45ad9c201bd424244e3572af9cee43464ca2
Rambod Azimi <59399542+rambodazimi@users.noreply.github.com>
2023-05-09 12:05:41 -0400

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,9 @@ import requests
 import base64
 from bs4 import BeautifulSoup
 import webbrowser
-
+# import warnings
+# warnings.filterwarnings("ignore")
+from selenium import webdriver
 
 # Author: Rambod Azimi
 # This Python script gets the IP address of the printer with username and password
@@ -10,9 +12,19 @@ import webbrowser
 # Print Server Settings --> Print Server --> Web Admin
 # Finally, it displays the content of that page (Admin name, password, http port, ...)
 
-url = 'http://192.168.1.93'
-username = 'admin'
-password = '1234'
+
+url = input("Please enter the IP address of the printer: ")
+url = 'http://' + url
+
+username = input("Please enter the username of the printer (empty for default): ")
+
+if(len(username) == 0):
+    username = 'admin'
+
+password = input("Please enter the password of the printer (empty for default): ")
+
+if(len(password) == 0):
+    password = '1234'
 
 credentials = f'{username}:{password}'.encode('utf-8')
 encoded_credentials = base64.b64encode(credentials).decode('utf-8')
@@ -43,13 +55,36 @@ if print_server_settings_link:
 
         if web_admin_link:
             web_admin_url = url + '/' + 'server/' + web_admin_link.get('href')
-            print(f"LINK = {web_admin_url}")
             webbrowser.open(web_admin_url)
             web_admin_response = requests.get(web_admin_url, headers=headers)
-            print(web_admin_response.content)
+           # print(web_admin_response.content)
         else:
             print('Web Admin link not found on Print Server page')
     else:
         print('Print Server link not found on Print Server Settings page')
 else:
     print('Print Server Settings link not found')
+
+
+# TODO change the password and submit changes
+""""
+isFound = False
+
+# Replace the string in the HTML content
+old_string = "<input type=password value=\"********\""
+new_string = "<input type=password value=\"RAMBOD\""
+html_content = web_admin_response.content.decode("utf-8")  # Convert bytes to string
+
+if(html_content.__contains__(old_string)):
+    isFound = True
+
+html_content = html_content.replace(old_string, new_string)
+
+# print(html_content)
+
+# Send a POST request to update the website (if applicable)
+web_admin_response = requests.post(web_admin_url, data=html_content, headers=headers)
+
+print(web_admin_response.text)
+
+"""
